8799f005f19adb06a3231da5805964d5b51e388f
SwapnilDwivedi77 <62413591+SwapnilDwivedi77@users.noreply.github.com>
2020-06-05 13:19:35 +0530

    added cart icon and cart dropdown component to application

---
+++ src/component/cart-dropdown/cart-dropdown.component.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './cart-dropdown.styles.scss'
+import CustomButton from '../custom-button/custom-button.component'
+
+const Cart = () => (
+
+    <div className='cart-dropdown'>
+
+        <div className='cart-iems'/>
+
+        <CustomButton>GO TO CHECKOUT</CustomButton>
+
+    </div>
+)
+
+export default Cart;
--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -4,8 +4,10 @@ import {ReactComponent as Logo} from '../../assets/crown.svg'
 import {auth} from '../../firebase/firebase.utils'
 import { connect } from  'react-redux'
 import './header.style.scss';
+import CartIcon from '../cart-icon/cart-icon.component'
+import Cart from '../cart-dropdown/cart-dropdown.component'
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser,hidden}) => (
 
     <div className='header'>
 
@@ -28,11 +30,18 @@ const Header = ({currentUser}) => (
             <Link className='option' to='/signin'>
             SIGN IN
             </Link>}
+            <CartIcon/>
+
         </div>
+        {
+            hidden ? null : <Cart/>
+        }
+        
 
     </div>
 )
-const mapStateToProps = state =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart : {hidden}}) =>({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header);
