6064a4f2bb9d284d1a866ebbda353c316f4a5b8a
SwapnilDwivedi77 <62413591+SwapnilDwivedi77@users.noreply.github.com>
2020-06-06 16:36:20 +0530

    implemented selectors for our cart

--- src/component/cart-dropdown/cart-dropdown.component.js
+++ src/component/cart-dropdown/cart-dropdown.component.js
@@ -3,6 +3,7 @@ import './cart-dropdown.styles.scss'
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component';
 import {connect} from 'react-redux'
+import {selectCartItems} from '../redux/cart/cart.selectors'
 
 const Cart = ({cartItems}) => (
 
@@ -19,8 +20,8 @@ const Cart = ({cartItems}) => (
     </div>
 )
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 
 })
 
